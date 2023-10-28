c60ed403da79d2d2c111569365a147c1f80bcf14
Zoe Yi <zoe.yi.25@bishops.com>
2023-10-28 21:45:09 +0000

    map root to classfive

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("classfive/", include("classfive.urls"))
+    path("", include("classfive.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
